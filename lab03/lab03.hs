9b6b0ded439b7c3a666d2d64d19c86c152ca689d
nightnoryu <qazwf734@gmail.com>
2024-01-27 16:57:25 +0300

    Finish lab03

--- lab03/lab03.hs
+++ lab03/lab03.hs
@@ -14,13 +14,20 @@ myunion [] b = b
 myunion a (x:xs) | elem x a  = myunion a xs
                  | otherwise = myunion (x:a) xs
 
--- mysubst :: Eq a => [a] -> [a] -> [a]
+mysubst :: Eq a => [a] -> [a] -> [a]
+mysubst a [] = a
+mysubst [] b = []
+mysubst (x:xs) (y:ys) | x == y    = mysubst xs (y:ys)
+                      | elem y xs = mysubst (xs ++ [x]) (y:ys)
+                      | otherwise = mysubst (x:xs) ys
 
--- getNthElementsList :: Int -> [[a]] -> [a]
+getNthElementsList :: Int -> [[a]] -> [a]
+getNthElementsList n l = map (\x -> x !! n) l
 
 main :: IO()
 main = do
   print(listnums 10)
   print(secondlastlist [[1, 2, 3], [4, 5, 6], [42, 56], [], [1]])
   print(myunion [1, 3, 4, 5] [1, 2, 4, 6])
-  -- print(mysubst [1, 2, 3, 4, 5] [1, 3, 6, 7, 5])
+  print(mysubst [1, 2, 3, 4, 5, 10] [1, 3, 6, 7, 5])
+  print(getNthElementsList 1 [[1, 2, 3], [4, 5, 6, 7], [9, 45]])
