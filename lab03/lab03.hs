dee22eda3b6dff568b01ff19dc46e61c4203a39b
nightnoryu <qazwf734@gmail.com>
2024-01-27 16:12:04 +0300

    lab03: add tasks 2 & 3

--- lab03/lab03.hs
+++ lab03/lab03.hs
@@ -1,15 +1,26 @@
 listnums :: Int -> [Int]
-listnums 1 = [1]
-listnums n = n : listnums (n - 1)
+listnums n | n <= 0    = []
+           | n == 1    = [1]
+           | otherwise = n : listnums (n - 1)
 
 secondlastlist :: [[a]] -> [a]
+secondlastlist [] = []
+secondlastlist (x:xs) | length x == 0 = secondlastlist xs
+                      | otherwise     = last x : secondlastlist xs
 
-myunion :: Eq a => [a] => [a] => [a]
+myunion :: Eq a => [a] -> [a] -> [a]
+myunion a [] = a
+myunion [] b = b
+myunion a (x:xs) | elem x a  = myunion a xs
+                 | otherwise = myunion (x:a) xs
 
-mysubst :: Eq a => [a] => [a] => [a]
+-- mysubst :: Eq a => [a] -> [a] -> [a]
 
-getNthElementsList :: Int -> [[a]] -> [a]
+-- getNthElementsList :: Int -> [[a]] -> [a]
 
 main :: IO()
 main = do
   print(listnums 10)
+  print(secondlastlist [[1, 2, 3], [4, 5, 6], [42, 56], [], [1]])
+  print(myunion [1, 3, 4, 5] [1, 2, 4, 6])
+  -- print(mysubst [1, 2, 3, 4, 5] [1, 3, 6, 7, 5])
