3f3422dda609ffdb097c33ae67230380dd11b579
nightnoryu <qazwf734@gmail.com>
2024-02-06 11:57:16 +0300

    lab05: replace handles with filenames in task 3
    Still bullshit

--- lab05/task3.hs
+++ lab05/task3.hs
@@ -3,71 +3,71 @@ import Control.Monad
 import Data.List
 import Data.Char
 
-mainLoop :: Handle -> IO()
-mainLoop handle = do
+mainLoop :: String -> IO()
+mainLoop filename = do
   putStr "> "
   cmd <- getLine
-  executeCmd cmd handle
-  if cmd /= "quit" then mainLoop handle else putStrLn "Finished"
+  executeCmd cmd filename
+  if cmd /= "quit" then mainLoop filename else putStrLn "Finished"
 
-executeCmd :: String -> Handle -> IO()
-executeCmd cmd handle | cmd == "print" = printFile handle
-                      | cmd == "add" = addLine handle
-                      | cmd == "remove" = removeLine handle
-                      | cmd == "fnl" = filterNewLines handle
-                      | cmd == "fp" = filterPunctuation handle
-                      | cmd == "quit" = putStrLn "Quit"
-                      | otherwise  = putStrLn "Unknown command"
+executeCmd :: String -> String -> IO()
+executeCmd cmd filename | cmd == "print" = printFile filename
+                        | cmd == "add" = addLine filename
+                        | cmd == "remove" = removeLine filename
+                        | cmd == "fnl" = filterNewLines filename
+                        | cmd == "fp" = filterPunctuation filename
+                        | cmd == "quit" = putStrLn "Quit"
+                        | otherwise  = putStrLn "Unknown command"
 
-printFile :: Handle -> IO()
-printFile handle = hGetContents handle >>= \contents -> putStr $ prependLineNumbers contents
+printFile :: String -> IO()
+printFile filename = do
+  contents <- readFile filename
+  putStr $ prependLineNumbers contents
   where
     prependLineNumbers :: String -> String
     prependLineNumbers text = unlines $ zipWith (\n line -> show n ++ ": " ++ line) [0..] (lines text)
 
-addLine :: Handle -> IO()
-addLine handle = do
+addLine :: String -> IO()
+addLine filename = do
   putStr "Enter line number to add a new line after: "
   n <- readNum
   putStr "Enter the new line: "
   line <- getLine
-  contents <- hGetContents handle
+  contents <- readFile filename
   let fileLines = lines contents
-  hPutStr handle (unlines $ take n fileLines ++ [line] ++ drop n fileLines)
+  writeFile filename (unlines $ take n fileLines ++ [line] ++ drop n fileLines)
   where
     readNum :: IO Int
     readNum = readLn
 
-removeLine :: Handle -> IO()
-removeLine handle = do
+removeLine :: String -> IO()
+removeLine filename = do
   putStr "Enter line number: "
   n <- readNum
-  contents <- hGetContents handle
+  contents <- readFile filename
   let fileLines = lines contents
-  hPutStr handle (unlines $ delete (fileLines !! n) fileLines)
+  writeFile filename (unlines $ delete (fileLines !! n) fileLines)
   where
     readNum :: IO Int
     readNum = readLn
 
-filterNewLines :: Handle -> IO()
-filterNewLines handle = do
+filterNewLines :: String -> IO()
+filterNewLines filename = do
   putStr "All new lines will be replaced with whitespaces\nEnter new file name: "
-  fileName <- getLine
-  contents <- hGetContents handle
-  writeFile fileName $ intercalate " " $ lines contents
+  dstFilename <- getLine
+  contents <- readFile filename
+  writeFile dstFilename $ intercalate " " $ lines contents
 
-filterPunctuation :: Handle -> IO()
-filterPunctuation handle = do
+filterPunctuation :: String -> IO()
+filterPunctuation filename = do
   putStr "All punctuation will be removed\nEnter new file name: "
-  fileName <- getLine
-  contents <- hGetContents handle
-  writeFile fileName $ filter (not . isPunctuation) contents
+  dstFilename <- getLine
+  contents <- readFile filename
+  writeFile dstFilename $ filter (not . isPunctuation) contents
 
 main :: IO()
 main = do
   hSetBuffering stdout NoBuffering
   putStr "Enter the name of the file to work with: "
-  fileName <- getLine
-  handle <- openFile fileName ReadWriteMode
-  mainLoop handle
-  hClose handle
+  filename <- getLine
+  mainLoop filename
