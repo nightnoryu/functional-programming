269e71b2b54473c6d8d3d650dc07c7a4e94c8984
nightnoryu <qazwf734@gmail.com>
2024-02-18 13:50:01 +0300

    lab10: add task 2

--- lab10/lab10.pl
+++ lab10/lab10.pl
@@ -16,6 +16,10 @@ vertical(N) :- seg(N, point(X, _), point(X, _)).
 % Задание 2
 
 seglength(N, L) :- seg(N, point(X1, Y1), point(X2, Y2)),
-  L is sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1)).
+  sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1), L).
 
-cross(N, M, point(X, Y), NL, ML) :-
+cross(N, M, point(X, Y), NL, ML) :- seg(N, point(X1, Y), point(X2, Y)), seg(M, point(X, Y3), point(X, Y4)),
+  X1 < X, X < X2,
+  Y4 < Y, Y < Y3,
+  seglength(N, NL),
+  seglength(M, ML).
